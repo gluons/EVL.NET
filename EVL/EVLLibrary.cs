c7d7ea3cd96e30d837274488ef1676a68606a1e2
Saran Tanpituckpong <sarunta@gmail.com>
2021-07-30 18:30:26 +0700

    🎉 Init

---
+++ EVL/EVLLibrary.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace EVL
+{
+	/// <summary>
+	/// EVL library.
+	/// </summary>
+	public static class EVLLibrary
+	{
+		/// <summary>
+		/// Return the value from fallback function if main function throws exception.
+		/// </summary>
+		/// <typeparam name="T">Value type</typeparam>
+		/// <param name="func">Main function</param>
+		/// <param name="fallbackFunc">Fallback function</param>
+		/// <returns>Actual value from successful function</returns>
+		public static T EVL<T>(Func<T> func, Func<T> fallbackFunc)
+		{
+			try
+			{
+				return func();
+			}
+			catch (Exception)
+			{
+				return fallbackFunc();
+			}
+		}
+
+		/// <summary>
+		/// Return the value from fallback function if main function throws exception.
+		/// </summary>
+		/// <typeparam name="T">Value type</typeparam>
+		/// <param name="func">Main function</param>
+		/// <param name="fallbackValue">Fallback value</param>
+		/// <returns>Actual value from successful function</returns>
+		public static T EVL<T>(Func<T> func, T fallbackValue)
+		{
+			try
+			{
+				return func();
+			}
+			catch (Exception)
+			{
+				return fallbackValue;
+			}
+		}
+	}
+}
